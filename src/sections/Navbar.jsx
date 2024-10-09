38e629840b856a022aa2e9c3969967211a9e2cfd
Sakib Hasan <sakib100.sa@gmail.com>
2024-10-09 12:05:03 +0600

    creating constants

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,4 +1,20 @@
+import { useState } from 'react';
+
+const NavItems = () => {
+    return (
+        <ul className='nav-ul'>
+            {['Home', 'About', 'Projects', 'Contact'].map((item, index) => (
+                <li key={index} className='nav-li'>
+                    {item}
+                </li>
+            ))}
+        </ul>
+    );
+};
+
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => setIsOpen((prev) => !prev);
     return (
         <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
             <div className='max-w-7xl mx-auto'>
@@ -9,13 +25,22 @@ const Navbar = () => {
                     >
                         Sakib
                     </a>
-                    <button>
+                    <button
+                        onClick={toggleMenu}
+                        className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex'
+                        aria-label={toggleMenu}
+                    >
                         <img
-                            src={'assets/menu.svg'}
+                            src={
+                                isOpen ? 'assets/close.svg' : 'assets/menu.svg'
+                            }
                             alt='toggle'
                             className='w-6 h-6 '
                         />
                     </button>
+                    <nav className='sm:flex hidden'>
+                        <NavItems />
+                    </nav>
                 </div>
             </div>
         </header>
