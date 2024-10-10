169dab98b42de5c71c0e266462359f8d0b403114
Sakib Hasan <sakib100.sa@gmail.com>
2024-10-10 11:55:21 +0600

    hacker room 3D model added to the hero section

--- src/constants/index.js
+++ src/constants/index.js
@@ -242,32 +242,32 @@ export const myProjects = [
     },
 ];
 
-export const calculateSizes = (isSmall, isMobile, isTablet) => {
+export const calculateSizes = (isSmallScreen, isMobile, isTablet) => {
     return {
-        deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+        deskScale: isSmallScreen ? 0.05 : isMobile ? 0.06 : 0.065,
         deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
-        cubePosition: isSmall
+        cubePosition: isSmallScreen
             ? [4, -5, 0]
             : isMobile
             ? [5, -5, 0]
             : isTablet
             ? [5, -5, 0]
             : [9, -5.5, 0],
-        reactLogoPosition: isSmall
+        reactLogoPosition: isSmallScreen
             ? [3, 4, 0]
             : isMobile
             ? [5, 4, 0]
             : isTablet
             ? [5, 4, 0]
             : [12, 3, 0],
-        ringPosition: isSmall
+        ringPosition: isSmallScreen
             ? [-5, 7, 0]
             : isMobile
             ? [-10, 10, 0]
             : isTablet
             ? [-12, 10, 0]
             : [-24, 10, 0],
-        targetPosition: isSmall
+        targetPosition: isSmallScreen
             ? [-5, -10, -10]
             : isMobile
             ? [-9, -10, -10]
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,45 +3,14 @@ import { PerspectiveCamera } from '@react-three/drei';
 import HackerRoom from '../components/HackerRoom';
 import { Suspense } from 'react';
 import CanvasLoader from '../components/CanvasLoader';
-import { Leva, useControls } from 'leva';
+import { useMediaQuery } from 'react-responsive';
+import { calculateSizes } from '../constants/index';
 const Hero = () => {
-    const controls = useControls('HackerRoom', {
-        positionX: {
-            value: 2.5,
-            min: -10,
-            max: 10,
-        },
-        positionY: {
-            value: 2.5,
-            min: -10,
-            max: 10,
-        },
-        positionZ: {
-            value: 2.5,
-            min: -10,
-            max: 10,
-        },
-        rotationX: {
-            value: 0,
-            min: -10,
-            max: 10,
-        },
-        rotationY: {
-            value: 0,
-            min: -10,
-            max: 10,
-        },
-        rotationZ: {
-            value: 0,
-            min: -10,
-            max: 10,
-        },
-        scale: {
-            value: 1,
-            min: 0.1,
-            max: 10,
-        },
-    });
+    const isSmallScreen = useMediaQuery({ maxWidth: 440 });
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+    const sizes = calculateSizes(isSmallScreen, isMobile, isTablet);
     return (
         <section className='min-h-screen w-full flex flex-col relative'>
             <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
@@ -53,29 +22,13 @@ const Hero = () => {
                 </p>
             </div>
             <div className='w-full h-full absolute inset-0'>
-                <Leva />
                 <Canvas className='w-full h-full'>
                     <Suspense fallback={<CanvasLoader />}>
-                        <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+                        <PerspectiveCamera makeDefault position={[0, 0, 20]} />
                         <HackerRoom
-                            // scale={0.07}
-                            // position={[0, 0, 0]}
-                            // rotation={[0, 280, 0]}
-                            position={[
-                                controls.positionX,
-                                controls.positionY,
-                                controls.positionZ,
-                            ]}
-                            rotation={[
-                                controls.rotationX,
-                                controls.rotationY,
-                                controls.rotationZ,
-                            ]}
-                            scale={[
-                                controls.scale,
-                                controls.scale,
-                                controls.scale,
-                            ]}
+                            position={sizes.deskPosition}
+                            rotation={[0, -Math.PI, 0]}
+                            scale={sizes.deskScale}
                         />
 
                         <ambientLight intensity={1} />
